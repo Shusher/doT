db3b599692f59318a8da23a5be42735a70bff59f
Artyom <s3dn@ya.ru>
2017-07-02 11:06:15 +0300

    test commit message

--- test/util.js
+++ test/util.js
@@ -1,6 +1,7 @@
 'use strict';
 
-var assert = require('assert')
+var assert = require('assert');
+var testChange = "hi!";
 var doT = require('../doT');
 
 exports.test = function (templates, data, result) {
